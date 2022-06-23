194809e4b0bf0927afca00c4d7c9c6e42dc0dfe6
Jeremiah Nkumsah <jnkumsah@jeremia2vm5f646.myfiosgateway.com>
2022-06-22 21:42:03 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#python sample project for circleci training
+
+
+def Add(a, b):
+        return a + b
+        
+def SayHello():
+        print("Hello World, from Terakeet!")
+
+if __name__ == '__main__':
+        SayHello()
+
+
